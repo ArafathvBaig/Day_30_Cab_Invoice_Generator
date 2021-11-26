f303bf65476ff665c6e28d8316ff44ded72153ca
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-26 16:38:16 +0530

    Added: Files With UC1

--- src/main/java/com/Day_30_Cab_Invoice_Generator/CabInvoiceGenerator.java
+++ src/main/java/com/Day_30_Cab_Invoice_Generator/CabInvoiceGenerator.java
@@ -4,11 +4,16 @@ public class CabInvoiceGenerator
 {
 	private static final int COST_PER_MINUTE = 1;
 	private static final double MINIMUM_COST_PER_KM = 10.0;
-
+	private static final double MIN_FARE = 5;
+	
+	
 	public double calculateFare(double distance, int time)
 	{
-		double fare = (distance * MINIMUM_COST_PER_KM) + (time * COST_PER_MINUTE);
-		return fare;
+		double totalFare = (distance * MINIMUM_COST_PER_KM) + (time * COST_PER_MINUTE);
+		if(totalFare <= MIN_FARE)
+		{
+			return MIN_FARE;
+		}
+		return totalFare;
 	}
-
 }
--- src/test/java/com/Day_30_Cab_Invoice_Generator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/Day_30_Cab_Invoice_Generator/CabInvoiceGeneratorTest.java
@@ -15,4 +15,14 @@ public class CabInvoiceGeneratorTest
 		double fare = cig.calculateFare(distance, time);
 		Assert.assertEquals(25, fare, 0.0);
 	}
+	
+	@Test
+	public void givenLessDistanceOrTime_ShouldReturnMinimumFare()
+	{
+		CabInvoiceGenerator cig = new CabInvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = cig.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
 }
