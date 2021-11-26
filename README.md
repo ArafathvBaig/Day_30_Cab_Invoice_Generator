d06e6dc18cb1e9178394c2dc8299eb9dac1da209
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-26 15:28:03 +0530

    Added: .gitignore and Welcome Files to Master

---
+++ README.md
@@ -0,0 +1,1 @@
+Day_30_Cab_Invoice_Generator
---
+++ src/main/java/com/Day_30_Cab_Invoice_Generator/WelcomeProgram.java
@@ -0,0 +1,9 @@
+package com.Day_30_Cab_Invoice_Generator;
+
+public class WelcomeProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome Cab Invoice Generator Computation Problem");
+	}
+}
